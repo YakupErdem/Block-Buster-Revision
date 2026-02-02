7ab43ce0c19856503044ee293b321c5c854171fb
Yakup Erdem Yılmaz <109314048+YakupErdem@users.noreply.github.com>
2026-02-02 18:20:07 +0300

    arrow remove

--- block-buster/src/scenes/Scene.ts
+++ block-buster/src/scenes/Scene.ts
@@ -68,7 +68,6 @@ export default class Scene extends Phaser.Scene {
 
 	// Visuals
 	private idleHexagon!: Phaser.GameObjects.Graphics;
-	private aimArrow!: Phaser.GameObjects.Graphics;
 	private trailEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
 	private impactEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
 	private explosionEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
@@ -665,54 +664,6 @@ export default class Scene extends Phaser.Scene {
 			this.idleHexagon.rotation += 0.005;
 		}
 
-		// Update Aim Arrow
-		if (!this.aimArrow) {
-			this.aimArrow = this.add.graphics();
-			this.aimArrow.setDepth(20); // Above background/enemies, below UI?
-		}
-		this.aimArrow.clear();
-		const cx = this.scale.width / 2;
-		const cy = this.scale.height / 2;
-
-		// Arrow styling: White fill, Black border, 3D-ish
-		// Angle is this.spiralAngle
-		// Length ~60px
-
-		const arrowLength = 60;
-		const tipX = cx + Math.cos(this.spiralAngle) * arrowLength;
-		const tipY = cy + Math.sin(this.spiralAngle) * arrowLength;
-
-		// Arrow Head
-		const headSize = 15;
-		const angle = this.spiralAngle;
-
-		// Vertices
-		const p1 = { x: tipX, y: tipY }; // Tip
-		const p2 = { x: tipX - headSize * Math.cos(angle - Math.PI / 6), y: tipY - headSize * Math.sin(angle - Math.PI / 6) };
-		const p3 = { x: tipX - headSize * Math.cos(angle + Math.PI / 6), y: tipY - headSize * Math.sin(angle + Math.PI / 6) };
-		const pCenter = { x: tipX - headSize * 0.5 * Math.cos(angle), y: tipY - headSize * 0.5 * Math.sin(angle) };
-
-		// Shaft
-		const startX = cx + Math.cos(angle) * 20; // Offset from center a bit
-		const startY = cy + Math.sin(angle) * 20;
-
-		this.aimArrow.lineStyle(4, 0x000000);
-		this.aimArrow.fillStyle(0xffffff);
-
-		// Draw Shaft Line
-		this.aimArrow.beginPath();
-		this.aimArrow.moveTo(startX, startY);
-		this.aimArrow.lineTo(pCenter.x, pCenter.y);
-		this.aimArrow.strokePath();
-
-		// Draw Arrow Head
-		this.aimArrow.beginPath();
-		this.aimArrow.moveTo(p1.x, p1.y);
-		this.aimArrow.lineTo(p2.x, p2.y);
-		this.aimArrow.lineTo(p3.x, p3.y);
-		this.aimArrow.closePath();
-		this.aimArrow.fillPath();
-		this.aimArrow.strokePath();
 
 		// Game Over Check: If we are not running updates or scene paused?
 		// Actually we just stop physics or ignore updates if game over.
